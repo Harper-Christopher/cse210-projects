c4efa23957f252a1ecdeac82ab2b8e2fd4750ee6
Christopher Harper <christopherharper123@gmail.com>
2024-02-28 22:00:40 -0700

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percent? ");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+        string letter;
+
+        if (gradePercent >= 90) 
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80) 
+        {
+            letter = "B"; 
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60) 
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+       
+        string sign;
+        int last_digit = gradePercent % 10;
+
+        if (last_digit >= 7)
+        {
+            sign = "+";
+        }
+        else if (last_digit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        
+        if (gradePercent >= 93)
+        {
+            sign = "";
+        }
+       
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Stay focused and you'll get it next time!");
+        }
     }
 }
