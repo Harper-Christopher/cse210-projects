a15414ec54e54edc8ae8d41a94534d80705d52a2
Christopher Harper <christopherharper123@gmail.com>
2024-02-28 22:30:06 -0700

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+
+        do
+        {
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int amountGuessed = 1;
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int numberGuessed = int.Parse(guess);
+
+        while (numberGuessed != magicNumber)
+        {
+            if (numberGuessed < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                amountGuessed = amountGuessed + 1;
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                numberGuessed = int.Parse(guess);
+            }
+
+            else if (numberGuessed > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                amountGuessed = amountGuessed + 1;
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                numberGuessed = int.Parse(guess);
+            }
+        }
+
+        Console.WriteLine($"You guessed it! The magic number was {magicNumber}!");
+        Console.WriteLine($"It took you {amountGuessed} guesses.");
+        Console.Write("Would you like to play again? (yes/no) ");
+        response = Console.ReadLine();
+        } while(response == "yes");
     }
 }
