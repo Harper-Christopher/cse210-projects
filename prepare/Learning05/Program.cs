29a3cdbdf036cddf542ede8b3a99a75a47a2319b
Christopher Harper <christopherharper123@gmail.com>
2024-03-26 19:39:01 -0600

    Learning 05 Finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Blue", 4));
+        shapes.Add(new Rectangle("Purple", 7, 10));
+        shapes.Add(new Circle("Green", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The shape is {color} with an area of {area}");
+        }
     }
 }
